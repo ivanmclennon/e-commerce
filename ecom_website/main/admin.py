555b8cefc6d0dc3da493080af00956bb7a7448ef
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-07-28 19:58:13 +0300

    registered models in admin

--- ecom_website/main/admin.py
+++ ecom_website/main/admin.py
@@ -1,17 +1,49 @@
 from django.contrib import admin
 from django import forms
+from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin
 
 from ckeditor.widgets import CKEditorWidget
 
-from .models import Category
+from .models import Category, Tag, Seller, Listing
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug',)
+    search_fields = ('title',)
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('username',)
+    list_display = ('id','username','email',)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    search_fields = ('user',)
+    list_display = ('user',)
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    search_fields = ('title','category','seller',)
+    list_display = (
+        'title',
+        'category',
+        'seller',
+        'description',
+        'date_created',
+        'date_modified',
+    )
 
 class FlatPageAdminForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorWidget())
