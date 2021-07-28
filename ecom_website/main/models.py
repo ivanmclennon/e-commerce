aa38c121d67fcf0538c2ded9309c832679271388
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-07-28 15:02:32 +0300

    added models to main

--- ecom_website/main/models.py
+++ ecom_website/main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Seller(models.Model):
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+
+    @property
+    def count_listings(self):
+        return self.listings.objects.count()
+
+
+class Tag(models.Model):
+    title = models.CharField()
+
+
+class Category(models.Model):
+    title = models.CharField()
+    slug = models.SlugField(auto_created=True)
+
+
+class Listing(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name='listings'
+    )
+    seller = models.ForeignKey(
+        to=Seller,
+        on_delete=models.CASCADE,
+        related_name='listings'
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(
+        to=Tag,
+        related_name='listings'
+    )
