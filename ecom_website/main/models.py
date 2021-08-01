0e83b7cd8c9342bdf5e5ddb1db5a1a709425b20f
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-01 15:45:07 +0300

    added new listing models, no migrations

--- ecom_website/main/models.py
+++ ecom_website/main/models.py
@@ -1,8 +1,12 @@
+from abc import ABC
+
 from django.db import models
 from django.contrib.auth.models import User
 
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
+from django_countries.fields import CountryField
 
 from .signals import unique_slug_generator
 
@@ -90,6 +94,93 @@ class Listing(models.Model):
     )
     price = models.PositiveIntegerField()
 
+    def __str__(self) -> str:
+        return self.title
+
     class Meta:
         verbose_name = 'listing'
         verbose_name_plural = 'listings'
+        abstract = True
+
+
+COLOR_CHOICES = (
+    ('WHITE', 'white'),
+    ('BLACK', 'black'),
+    ('GREY', 'grey'),
+    ('RED', 'red'),
+    ('GREEN', 'green'),
+    ('BLUE', 'blue'),
+    ('YELLOW', 'yellow'),
+    ('PURPLE', 'purple'),
+    ('ORANGE', 'orange'),
+    ('PINK', 'pink'),
+)
+
+
+class ItemListing(Listing):
+    """
+    Unspecified general item class.
+    :param weight: weight in kg 0.01
+    :param made_in: manufacturer country
+    :param color: item color
+    """
+
+    # specify validation constraints in the Form
+    weight = models.FloatField()
+    # use CountrySelectWidget in Form
+    made_in = CountryField(blank_label='(select country)')
+    color = models.CharField(
+        max_length=16,
+        choices=COLOR_CHOICES,
+        default="WHITE"
+    )
+
+    class Meta:
+        verbose_name = 'item'
+        verbose_name_plural = 'items'
+
+
+class AutoListing(Listing):
+
+    # specify validation constraints in the Form
+    weight = models.FloatField()
+    # use CountrySelectWidget in Form
+    made_in = CountryField(blank_label='(select country)')
+    color = models.CharField(
+        max_length=16,
+        choices=COLOR_CHOICES,
+        default="WHITE"
+    )
+
+    class Condition(models.TextChoices):
+        NEW = 'NEW', _('New')
+        USED = 'USED', _('Used')
+
+    condition = models.CharField(
+        max_length=8,
+        choices=Condition.choices,
+        default=Condition.NEW
+    )
+    # specify constraints in Form
+    mileage = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'auto'
+        verbose_name_plural = 'autos'
+
+
+class ServiceListing(Listing):
+
+    PLACE_TYPE_CHOICES = (
+        ('ONLINE', 'online'),
+        ('IRL', 'in person'),
+    )
+    place_type = models.CharField(
+        max_length=16,
+        choices=PLACE_TYPE_CHOICES,
+        default='IRL'
+    )
+
+    class Meta:
+        verbose_name = 'service'
+        verbose_name_plural = 'services'
