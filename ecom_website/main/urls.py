286824d3453cffb60bcaf7d6687597055717140e
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-03 22:01:24 +0300

    added remaining detail views and templates

--- ecom_website/main/urls.py
+++ ecom_website/main/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path, include
 
-from .views import index, ItemList, AutoList, ServiceList, ItemDetail
+from .views import (
+    index,
+    ItemList, AutoList, ServiceList,
+    ItemDetail, AutoDetail, ServiceDetail
+)
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('items/', ItemList.as_view()),
-    path('cars/', AutoList.as_view()),
-    path('services/', ServiceList.as_view()),
-    path('items/<int:pk>', ItemDetail.as_view()),
+    path('items/', ItemList.as_view(), name='items_list'),
+    path('cars/', AutoList.as_view(), name='cars_list'),
+    path('services/', ServiceList.as_view(), name='services_list'),
+    path('items/<int:pk>', ItemDetail.as_view(), name='item_detail'),
+    path('cars/<int:pk>', AutoDetail.as_view(), name='car_detail'),
+    path('services/<int:pk>', ServiceDetail.as_view(), name='service_detail'),
 ]
--- ecom_website/main/views.py
+++ ecom_website/main/views.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 
 from django.shortcuts import render
-from django.conf import settings
 from django.views.generic import ListView, DetailView
 
 from .models import ItemListing, AutoListing, ServiceListing
@@ -14,16 +13,22 @@ def index(request):
 class ItemList(ListView):
     model = ItemListing
 
+
 class ItemDetail(DetailView):
     model = ItemListing
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        return context
 
 class AutoList(ListView):
     model = AutoListing
 
 
+class AutoDetail(DetailView):
+    model = AutoListing
+
+
 class ServiceList(ListView):
+    model = ServiceListing
+
+
+class ServiceDetail(DetailView):
     model = ServiceListing
