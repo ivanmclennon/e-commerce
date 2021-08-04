6a970a3c566114b5d52ab04bd66fcc31115c922c
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-04 19:32:27 +0300

    added tags filtering with pagination for items listview

--- ecom_website/main/views.py
+++ ecom_website/main/views.py
@@ -1,9 +1,11 @@
 from typing import Any, Dict
+from urllib.parse import urlencode
 
+from django.db.models import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import ItemListing, AutoListing, ServiceListing
+from .models import ItemListing, AutoListing, ServiceListing, Tag
 
 
 def index(request):
@@ -15,6 +17,19 @@ class ItemList(ListView):
     model = ItemListing
     paginate_by = 3
 
+    def get_queryset(self) -> QuerySet[ItemListing]:
+        if 'tag' in self.request.GET:
+            return ItemListing.objects.filter(
+                tags__title=self.request.GET['tag']
+            )
+        return ItemListing.objects.all()
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        query_params = self.request.GET.copy()
+        query_params.pop('page', None)
+        context['query_params'] = urlencode(query_params)
+        return context
 
 class ItemDetail(DetailView):
     model = ItemListing
