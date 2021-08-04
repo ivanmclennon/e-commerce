cd7c02e01d8b3d80b553658efc12fbdc075e03e0
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-04 20:30:35 +0300

    added query_remainder tag

--- ecom_website/main/templatetags/main_tags.py
+++ ecom_website/main/templatetags/main_tags.py
@@ -6,12 +6,19 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-# custom now
+
 @register.simple_tag
 def current_time(format_string):
     return dt.now().strftime(format_string)
 
-# custom reversed
+
+@register.simple_tag(takes_context=True)
+def query_remainder(context):
+    if 'query_params' in context and context['query_params']:
+        return '&'+context['query_params']
+    return ''
+
+
 @register.filter(name="reversed")
 @stringfilter
 def custom_reversed(value):
