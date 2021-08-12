851c56b20efc1a1e8d3e5bcd421496dca629c426
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-12 17:37:03 +0300

    added docs to validators

--- ecom_website/main/validators.py
+++ ecom_website/main/validators.py
@@ -4,6 +4,9 @@ from django.core.exceptions import ValidationError
 
 
 def age_validator(birthday: date) -> None:
+    """
+    Raises ValidationError if full age is under 18 years
+    """
     today = date.today()
     age = today.year - birthday.year
     if (today.month < birthday.month) or (
@@ -15,5 +18,8 @@ def age_validator(birthday: date) -> None:
 
 
 def weight_validator(weight: float) -> None:
+    """
+    Raises ValidationError if weight is outside of (0.01, 9999.99) range
+    """
     if weight < 0.01 or weight > 9999.99:
         raise ValidationError("Weight must be between 0.01 and 9999.99")
