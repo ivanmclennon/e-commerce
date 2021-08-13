4514afae566f2d4825c8df2cfb10f4ea6dd07bb1
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-13 14:29:21 +0300

    moved accounts url to main app

--- ecom_website/ecom_website/urls.py
+++ ecom_website/ecom_website/urls.py
@@ -23,7 +23,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('accounts/', include('allauth.urls')),
     path('', include('main.urls')),
-    path('logout', LogoutView.as_view()),
+    path('logout', LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
