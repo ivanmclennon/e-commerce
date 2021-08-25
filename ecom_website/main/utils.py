2e9bc18ca40c984546a4cfa08ba6a9a66c845b39
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-25 23:29:55 +0300

    moved account adapter to users app

--- ecom_website/main/utils.py
+++ ecom_website/main/utils.py
@@ -5,22 +5,6 @@ from unidecode import unidecode as ud
 
 from django.utils.text import slugify
 
-from allauth.account.adapter import DefaultAccountAdapter
-
-from .models import Seller
-
-
-class SellerAccountAdapter(DefaultAccountAdapter):
-    """
-    Allauth account adapter for Seller model
-    """
-
-    def new_user(self, request):
-        """
-        Returns Seller instance
-        """
-        return Seller()
-
 
 def random_string_generator(
     size: int = 10, chars: str = string.ascii_lowercase + string.digits
