971ca5b2d28d47de52105bf9ba64d4c4c129fb5f
Ivan Gninenko <iagninenko@edu.hse.ru>
2021-08-26 21:41:09 +0300

    moved picture model back to main app

--- ecom_website/main/models.py
+++ ecom_website/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from listings.models import AutoListing
+
 
 class Tag(models.Model):
     """
@@ -35,3 +37,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+
+class Picture(models.Model):
+    image = models.ImageField(upload_to="main/cars", default="main/cars/NO_IMAGE.jpg")
+    car = models.ForeignKey(
+        to=AutoListing, on_delete=models.CASCADE, related_name="%(class)s_set"
+    )
+
+    def __str__(self) -> str:
+        return self.image.path
+
+    class Meta:
+        verbose_name = "picture"
+        verbose_name_plural = "pictures"
